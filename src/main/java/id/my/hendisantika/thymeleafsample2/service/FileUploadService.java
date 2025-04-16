190e324ffce890656b52d8b3f513c37995bd7594
hendisantika <hendisantika@yahoo.co.id>
2025-04-16 07:19:25 +0700

    feat: Add FileUploadService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/thymeleafsample2/service/FileUploadService.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.thymeleafsample2.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-sample2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/04/25
+ * Time: 07.19
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileUploadService {
+}
