849bf2823c0b577ed5921c3cec33f894c6568bc1
hendisantika <hendisantika@yahoo.co.id>
2025-04-16 07:16:37 +0700

    feat: Add Customer Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/thymeleafsample2/model/Customer.java
@@ -0,0 +1,63 @@
+package id.my.hendisantika.thymeleafsample2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-sample2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/04/25
+ * Time: 07.15
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity(name = "Customer")
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    @NotEmpty(message = "First Name is required")
+    @Size(min = 5, message = "First name should be at least 5 characters")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Last Name is required")
+    @Size(min = 5, message = "Last name should be at least 5 characters")
+    private String lastName;
+
+    @Column(name = "email")
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please enter a valid email address")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @Transient
+    private String photosImagePath;
+}
