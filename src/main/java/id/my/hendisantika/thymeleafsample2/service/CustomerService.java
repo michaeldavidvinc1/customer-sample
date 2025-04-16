cb54fb5d32f897c668806ea6121c54ebf83dba70
hendisantika <hendisantika@yahoo.co.id>
2025-04-16 07:18:23 +0700

    feat: Add CustomerService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/thymeleafsample2/service/CustomerService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.thymeleafsample2.service;
+
+import id.my.hendisantika.thymeleafsample2.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-sample2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/04/25
+ * Time: 07.17
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class CustomerService {
+    private CustomerRepository customerRepository;
+
+}
