69d5f4365ac060ffb5518e9bc73592e60720355b
hendisantika <hendisantika@yahoo.co.id>
2025-04-16 07:10:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/thymeleafsample2/SpringBootThymeleafSample2Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.thymeleafsample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafSample2Application.class, args);
+    }
+
+}
