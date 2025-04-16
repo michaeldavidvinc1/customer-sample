81d1e6762e0e8d1fdfe07de5a4baf0a92b05ea8f
hendisantika <hendisantika@yahoo.co.id>
2025-04-16 07:55:15 +0700

    fix: Integration Test🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/thymeleafsample2/SpringBootThymeleafSample2ApplicationTests.java
+++ src/test/java/id/my/hendisantika/thymeleafsample2/SpringBootThymeleafSample2ApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.thymeleafsample2;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.thymeleafsample2.repository.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:9.0:///customer_db"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootThymeleafSample2ApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        customerRepository.deleteAll();
     }
 
 }
