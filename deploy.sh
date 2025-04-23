e223402974d23035af362c5488022b6827eff81e
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 17:06:25 +0700

    fix: k8s deployment :one: :three:🫸🌀✏️📗🐧🐳

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,9 @@ if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     docker rm $CONTAINER_NAME
 fi
 
-docker run -d --restart unless-stopped --platform linux/amd64 -p $APP_SERVER_PORT:$APP_SERVER_PORT --env-file .env --name $CONTAINER_NAME  hendisantika/$IMAGE_NAME:$IMAGE_TAG
+#docker run -d --restart unless-stopped --platform linux/amd64 -p $APP_SERVER_PORT:$APP_SERVER_PORT --env-file .env --name $CONTAINER_NAME  hendisantika/$IMAGE_NAME:$IMAGE_TAG
+docker login $CLOUDRAYA_REGISTRY_URL -u $CLOUDRAYA_REGISTRY_USERNAME -p $CLOUDRAYA_REGISTRY_PASSWORD
+docker run -d --restart unless-stopped --platform linux/amd64 -p $APP_SERVER_PORT:$APP_SERVER_PORT --env-file .env --name $CONTAINER_NAME  $CLOUDRAYA_REGISTRY_URL/$CONTAINER_NAME:$IMAGE_TAG
 exit
 ENDSSH
 
